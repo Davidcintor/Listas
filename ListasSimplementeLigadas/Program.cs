f7e5a8e563d0f11402c36f0f9e016571daa92a2b
Davidcintor <David_Ramirez23@unikino.edu.mx>
2024-04-25 00:24:30 -0700

    Listas Simplemente Ligadas Terminada:)

--- ListasSimplementeLigadas/Program.cs
+++ ListasSimplementeLigadas/Program.cs
@@ -1,7 +1,25 @@
 ﻿using ListasSimplementeLigadas;
-Nodo nodoVacio = new Nodo();
-Nodo nodo = new Nodo("Pepe");
 
-Console.WriteLine(nodoVacio.Valor);
-Console.WriteLine(nodo.Valor);
-//Console.WriteLine(nodo.Nombre);
+
+Lista miLista = new Lista();
+miLista.Agregar("cero");
+miLista.Agregar("uno");
+miLista.Agregar("dos");
+miLista.Agregar("tres");
+miLista.Agregar("cuatro");
+
+string elementos = miLista.Recorrer();
+Console.WriteLine(elementos);
+
+Console.WriteLine();
+Nodo encontrado = miLista.Buscar("dos");
+Console.WriteLine(encontrado.Valor);
+
+Console.WriteLine();
+Nodo encontrado2 = miLista.BuscarNodoAnterior("dos");
+Console.WriteLine(encontrado2.Valor);
+
+Console.WriteLine();
+miLista.Borrar("dos");
+elementos = miLista.Recorrer();
+Console.WriteLine(elementos);
