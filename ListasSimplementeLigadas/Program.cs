dbc457da09ee81de7bf5485c7b2200eeb5eb01e0
Davidcintor <David_Ramirez23@unikino.edu.mx>
2024-04-24 22:59:31 -0700

    Add project files.

---
+++ ListasSimplementeLigadas/Program.cs
@@ -0,0 +1,7 @@
+﻿using ListasSimplementeLigadas;
+Nodo nodoVacio = new Nodo();
+Nodo nodo = new Nodo("Pepe");
+
+Console.WriteLine(nodoVacio.Valor);
+Console.WriteLine(nodo.Valor);
+//Console.WriteLine(nodo.Nombre);
